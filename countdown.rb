89901c29e6bc3ff41c42d00263bbfa327a58f52c
Kevin Price <collegekevin@gmail.com>
2023-02-02 13:47:31 -0800

    commit tree 9602624642399dc8683b2e0da5aa67eec91b4f4f

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(arg)
+    while arg > 0
+        puts "#{arg} SECOND(S)!"
+        arg -= 1
+    end
+    return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(arg)
+    while arg > 0
+        puts "#{arg} SECOND(S)!"
+        sleep(1)
+        arg -= 1
+    end
+    return "HAPPY NEW YEAR!"
 end
